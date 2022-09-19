d980330869735029715eb7f4ce5780c3f7f0bb3e
lautarolombardi <lautaroalombardi22@gmail.com>
2022-09-19 11:27:32 -0300

    interactuar-con-html

--- main.js
+++ main.js
@@ -23,6 +23,7 @@ function simularCredito(){
         let interes;
         let total;
         let montoCuota;
+        let calc = document.getElementById('calc');
     
         do{
             montoSolicitado = parseInt(prompt('Ingrese el monto que desea solicitar'));
@@ -54,75 +55,70 @@ function simularCredito(){
                 interes = (montoSolicitado * 15) / 100;
                 total = montoSolicitado + interes;
                 montoCuota = Math.round(total / cuotas);
-                if(confirm(`El monto total a pagar es $${total}\n\nEn ${cuotas} cuotas de $${montoCuota}\n\n¿Desea continuar?`)){
-                    credito = {montoSolicitado, cuotas, interes, total, montoCuota};
-                    usuario.push(nombre, dni, credito);
-                    usuarioGuardado = usuario;
-                    console.log(usuario);
-                    if(confirm('La solicitud fue ingresada exitosamente. El monto se acreditará en un plazo máximo de 48 hs habiles\n\n¿Desea volver al menú principal?')){
-                        simularCredito();
-                    }
-                }
+
+                credito = {montoSolicitado, cuotas, interes, total, montoCuota};
+                usuario.push(nombre, dni, credito);
+                usuarioGuardado = usuario;
+
+                calc.classList = 'my-3'
+                calc.innerText += `El monto total a pagar es $${total}\nEn ${cuotas} cuotas de $${montoCuota}`;
+                
                 break;
             case 2:
                 cuotas = 24;
                 interes = (montoSolicitado * 30) / 100;
                 total = montoSolicitado + interes;
                 montoCuota = Math.round(total / cuotas);
-                if(confirm(`El monto total a pagar es $${total}\n\nEn ${cuotas} cuotas de $${montoCuota}\n\n¿Desea continuar?`)){
-                    credito = {montoSolicitado, cuotas, interes, total, montoCuota};
-                    usuario.push(nombre, dni, credito);
-                    usuarioGuardado = usuario;
-                    console.log(usuario);
-                    if(confirm('La solicitud fue ingresada exitosamente. El monto se acreditará en un plazo máximo de 48 hs habiles\n\n¿Desea volver al menú principal?')){
-                        simularCredito();
-                    }
-                }
+
+                credito = {montoSolicitado, cuotas, interes, total, montoCuota};
+                usuario.push(nombre, dni, credito);
+                usuarioGuardado = usuario;
+
+                calc.classList = 'my-3'
+                calc.innerText += `El monto total a pagar es $${total}\nEn ${cuotas} cuotas de $${montoCuota}`;
+
                 break;
             case 3:
                 cuotas = 36;
                 interes = (montoSolicitado * 45) / 100;
                 total = montoSolicitado + interes;
                 montoCuota = Math.round(total / cuotas);
-                if(confirm(`El monto total a pagar es $${total}\n\nEn ${cuotas} cuotas de $${montoCuota}\n\n¿Desea continuar?`)){
-                    credito = {montoSolicitado, cuotas, interes, total, montoCuota};
-                    usuario.push(nombre, dni, credito);
-                    usuarioGuardado = usuario;
-                    console.log(usuario);
-                    if(confirm('La solicitud fue ingresada exitosamente. El monto se acreditará en un plazo máximo de 48 hs habiles\n\n¿Desea volver al menú principal?')){
-                        simularCredito();
-                    }
-                }
+
+                credito = {montoSolicitado, cuotas, interes, total, montoCuota};
+                usuario.push(nombre, dni, credito);
+                usuarioGuardado = usuario;
+
+                calc.classList = 'my-3'
+                calc.innerText += `El monto total a pagar es $${total}\nEn ${cuotas} cuotas de $${montoCuota}`;
+                
                 break;
             case 4:
                 cuotas = 48;
                 interes = (montoSolicitado * 60) / 100;
                 total = montoSolicitado + interes;
                 montoCuota = Math.round(total / cuotas);
-                if(confirm(`El monto total a pagar es $${total}\n\nEn ${cuotas} cuotas de $${montoCuota}\n\n¿Desea continuar?`)){
-                    credito = {montoSolicitado, cuotas, interes, total, montoCuota};
-                    usuario.push(nombre, dni, credito);
-                    usuarioGuardado = usuario;
-                    console.log(usuario);
-                    if(confirm('La solicitud fue ingresada exitosamente. El monto se acreditará en un plazo máximo de 48 hs habiles\n\n¿Desea volver al menú principal?')){
-                        simularCredito();
-                    }
-                }
+
+                credito = {montoSolicitado, cuotas, interes, total, montoCuota};
+                usuario.push(nombre, dni, credito);
+                usuarioGuardado = usuario;
+
+                calc.classList = 'my-3'
+                calc.innerText += `El monto total a pagar es $${total}\nEn ${cuotas} cuotas de $${montoCuota}`;
+
                 break;
             case 5:
                 cuotas = 60;
                 interes = (montoSolicitado * 75) / 100;
                 total = montoSolicitado + interes;
                 montoCuota = Math.round(total / cuotas);
-                if(confirm(`El monto total a pagar es $${total}\n\nEn ${cuotas} cuotas de $${montoCuota}\n\n¿Desea continuar?`)){
-                    credito = {montoSolicitado, cuotas, interes, total, montoCuota};
-                    usuario.push(nombre, dni, credito);
-                    usuarioGuardado = usuario;
-                    console.log(usuario);
-                    if(confirm('La solicitud fue ingresada exitosamente. El monto se acreditará en un plazo máximo de 48 hs habiles\n\n¿Desea volver al menú principal?')){
-                        simularCredito();
-                    }
-                }
+
+                credito = {montoSolicitado, cuotas, interes, total, montoCuota};
+                usuario.push(nombre, dni, credito);
+                usuarioGuardado = usuario;
+
+                calc.classList = 'my-3'
+                calc.innerText += `El monto total a pagar es $${total}\nEn ${cuotas} cuotas de $${montoCuota}`;
+
                 break;
         }
     }
@@ -130,219 +126,6 @@ function simularCredito(){
 }
 
 simularCredito();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* function simularCredito(){
-    const usuario = [];
-    let usuario = {};
-    let nombre = '';
-    let dni;
-    let montoSolicitado;
-    let opcionCuotas;
-
-    alert('TU CRÉDITO');
-
-    do{
-        nombre = prompt('Ingresá tu nombre completo');
-    } while(nombre === '' || nombre === null)
-    
-    do{
-        dni = parseInt(prompt('Ingresá tu DNI'));
-    } while(isNaN(dni) || dni === '')
-    
-    do{
-        montoSolicitado = parseInt(prompt('Ingresá el monto a solicitar'));
-    } while(isNaN(montoSolicitado))
-    
-    while(montoSolicitado < 5000){
-        montoSolicitado = parseInt(prompt('El monto solicitado debe ser mayor a $5000'));
-    }
-    
-    while((montoSolicitado % 500) != 0){
-        montoSolicitado = parseInt(prompt('El monto solicitado debe ser múltiplo de $500'));
-    }
-    
-    do{
-        opcionCuotas = parseInt(prompt(
-            `¿En cuántas cuotas?\n
-            1. 12 cuotas\n
-            2. 24 cuotas\n
-            3. 36 cuotas\n
-            4. 48 cuotas\n
-            5. 60 cuotas\n
-            Seleccioná una opción`
-        ));
-    } while(opcionCuotas < 1 || opcionCuotas > 5 || isNaN(opcionCuotas))
-    
-    function calcularMontos(){
-        let cuotas;
-        let interes;
-        let total;
-        let montoCuota;
-        let aceptaConfirm2;
-        let aceptaConfirm3;
-        switch(opcionCuotas){
-            case 1: {
-                cuotas = 12;
-                interes = (montoSolicitado * 15) / 100;
-                total = montoSolicitado + interes;
-                montoCuota = Math.round(total / cuotas);
-    
-                aceptaConfirm2 = confirm(`
-                    El total a pagar es ${total}\n
-                    En ${cuotas} cuotas de ${montoCuota}\n
-                    ¿Deseas continuar?`
-                );
-                if(aceptaConfirm2){
-                    usuario = {nombre, dni, montoSolicitado, cuotas, interes, total, montoCuota};
-                    usuario.push(usuario);
-                    console.log(usuario);
-                    aceptaConfirm3 = confirm('Se te ha otorgado el crédito con éxito\n\n¿Volver a la pantalla de inicio?');
-                    if(aceptaConfirm3){
-                        simularCredito();
-                    }
-                } else{
-                    aceptaConfirm3 = confirm('Cancelaste la operación\n\n¿Volver a la pantalla de inicio?');
-                    if(aceptaConfirm3){
-                        simularCredito();
-                    }
-                }
-                break;
-            }
-            case 2: {
-                cuotas = 24;
-                interes = (montoSolicitado * 30) / 100;
-                total = montoSolicitado + interes;
-                montoCuota = Math.round(total / cuotas);
-    
-                aceptaConfirm2 = confirm(`
-                    El total a pagar es ${total}\n
-                    En ${cuotas} cuotas de ${montoCuota}\n
-                    ¿Deseas continuar?`
-                );
-                if(aceptaConfirm2){
-                    usuario = {nombre, dni, montoSolicitado, cuotas, interes, total, montoCuota};
-                    usuario.push(usuario);
-                    console.log(usuario);
-                    aceptaConfirm3 = confirm('Se te ha otorgado el crédito con éxito\n\n¿Volver a la pantalla de inicio?');
-                    if(aceptaConfirm3){
-                        simularCredito();
-                    }
-                } else{
-                    aceptaConfirm3 = confirm('Cancelaste la operación\n\n¿Volver a la pantalla de inicio?');
-                    if(aceptaConfirm3){
-                        simularCredito();
-                    }
-                }
-                break;
-            }
-            case 3: {
-                cuotas = 36;
-                interes = (montoSolicitado * 45) / 100;
-                total = montoSolicitado + interes;
-                montoCuota = Math.round(total / cuotas);
-    
-                aceptaConfirm2 = confirm(`
-                    El total a pagar es ${total}\n
-                    En ${cuotas} cuotas de ${montoCuota}\n
-                    ¿Deseas continuar?`
-                );
-                if(aceptaConfirm2){
-                    usuario = {nombre, dni, montoSolicitado, cuotas, interes, total, montoCuota};
-                    usuario.push(usuario);
-                    console.log(usuario);
-                    aceptaConfirm3 = confirm('Se te ha otorgado el crédito con éxito\n\n¿Volver a la pantalla de inicio?');
-                    if(aceptaConfirm3){
-                        simularCredito();
-                    }
-                } else{
-                    aceptaConfirm3 = confirm('Cancelaste la operación\n\n¿Volver a la pantalla de inicio?');
-                    if(aceptaConfirm3){
-                        simularCredito();
-                    }
-                }
-                break;
-            }
-            case 4: {
-                cuotas = 48;
-                interes = (montoSolicitado * 60) / 100;
-                total = montoSolicitado + interes;
-                montoCuota = Math.round(total / cuotas);
-    
-                aceptaConfirm2 = confirm(`
-                    El total a pagar es ${total}\n
-                    En ${cuotas} cuotas de ${montoCuota}\n
-                    ¿Deseas continuar?`
-                );
-                if(aceptaConfirm2){
-                    usuario = {nombre, dni, montoSolicitado, cuotas, interes, total, montoCuota};
-                    usuario.push(usuario);
-                    console.log(usuario);
-                    aceptaConfirm3 = confirm('Se te ha otorgado el crédito con éxito\n\n¿Volver a la pantalla de inicio?');
-                    if(aceptaConfirm3){
-                        simularCredito();
-                    }
-                } else{
-                    aceptaConfirm3 = confirm('Cancelaste la operación\n\n¿Volver a la pantalla de inicio?');
-                    if(aceptaConfirm3){
-                        simularCredito();
-                    }
-                }
-                break;
-            }
-            case 5: {
-                cuotas = 60;
-                interes = (montoSolicitado * 75) / 100;
-                total = montoSolicitado + interes;
-                montoCuota = Math.round(total / cuotas);
-    
-                aceptaConfirm2 = confirm(`
-                    El total a pagar es ${total}\n
-                    En ${cuotas} cuotas de ${montoCuota}\n
-                    ¿Deseas continuar?`
-                );
-                if(aceptaConfirm2){
-                    usuario = {nombre, dni, montoSolicitado, cuotas, interes, total, montoCuota};
-                    usuario.push(usuario);
-                    console.log(usuario);
-                    aceptaConfirm3 = confirm('Se te ha otorgado el crédito con éxito\n\n¿Volver a la pantalla de inicio?');
-                    if(aceptaConfirm3){
-                        simularCredito();
-                    }
-                } else{
-                    aceptaConfirm3 = confirm('Cancelaste la operación\n\n¿Volver a la pantalla de inicio?');
-                    if(aceptaConfirm3){
-                        simularCredito();
-                    }
-                }
-                break;
-            }
-        }
-    }
-    calcularMontos();
-}
-
-simularCredito(); */
 
 
 
