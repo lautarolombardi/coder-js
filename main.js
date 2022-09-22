ceeb638355db4fec734a585340a5cd487b26a1d7
Lautaro Lombardi <lautaroalombardi22@gmail.com>
2022-09-21 22:32:57 -0300

    incorporar-eventos

--- main.js
+++ main.js
@@ -1,53 +1,161 @@
-let usuarioGuardado = [];
+let form = document.querySelector('#form');
+let simulador = document.querySelector('#simulador');
+let btnReset = document.querySelector('#reset');
+let btnCalc = document.querySelector('#calc');
+
+let calcAgain = false;
+
+function validarForm(e){
+    e.preventDefault();
+}
+form.addEventListener('submit', validarForm);
+
+function calcularMontos(){
+    let montoSolicitado = parseInt(document.querySelector('#monto').value);
+    let cuotas = parseInt(document.querySelector('#cuotas').value);
+
+    let interes;
+    let total;
+    let montoCuota;
+
+    switch(cuotas){
+        case 12: {
+            interes = (montoSolicitado * 15) / 100;
+            total = montoSolicitado + interes;
+            montoCuota = Math.round(total / cuotas);
+
+            simulador.innerText = `El monto total a pagar es $${total}\nEn ${cuotas} cuotas de $${montoCuota}`;
+
+            calcAgain = true;
+            break;
+        }
+        case 24: {
+            interes = (montoSolicitado * 30) / 100;
+            total = montoSolicitado + interes;
+            montoCuota = Math.round(total / cuotas);
+
+            simulador.innerText = `El monto total a pagar es $${total}\nEn ${cuotas} cuotas de $${montoCuota}`; 
+
+            calcAgain = true;;
+            break;
+        }
+        case 36: {
+            interes = (montoSolicitado * 45) / 100;
+            total = montoSolicitado + interes;
+            montoCuota = Math.round(total / cuotas);
+
+            simulador.innerText = `El monto total a pagar es $${total}\nEn ${cuotas} cuotas de $${montoCuota}`; 
+
+            calcAgain = true;
+            break;
+        }
+        case 48: {
+            interes = (montoSolicitado * 60) / 100;
+            total = montoSolicitado + interes;
+            montoCuota = Math.round(total / cuotas);
+
+            simulador.innerText = `El monto total a pagar es $${total}\nEn ${cuotas} cuotas de $${montoCuota}`;
+            
+            calcAgain = true;
+            break;
+        }
+        case 60: {
+            interes = (montoSolicitado * 75) / 100;
+            total = montoSolicitado + interes;
+            montoCuota = Math.round(total / cuotas);
+
+            simulador.innerText = `El monto total a pagar es $${total}\nEn ${cuotas} cuotas de $${montoCuota}`; 
+
+            calcAgain = true;
+            break;
+        }
+    }
+}
+
+btnReset.addEventListener('click', (e)=>{
+    simulador.innerText = '';
+    calcAgain = false;
+});
+
+btnCalc.addEventListener('click', (e)=>{
+    if(calcAgain === false){
+        calcularMontos();
+    }  
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const nombre = document.getElementById('name');
+const dni = document.getElementById('dni');
 
 function simularCredito(){ 
     const usuario = [];
     let credito = {};
     let nombre = '';
-    let dni; 
-
-    alert('TU CRÉDITO');
-
-    do{
-        nombre = prompt('Ingrese tu nombre completo');
-    } while(nombre === '' || nombre === null)
-    
-    do{
-        dni = parseInt(prompt('Ingrese su DNI'));
-    } while(isNaN(dni))
+    let dni;
     
     function calcularMontos(){
-        let montoSolicitado;
+        const montoSolicitado = document.getElementById('monto');
         let opcionCuotas;
         let cuotas;
         let interes;
         let total;
         let montoCuota;
-        let calc = document.getElementById('calc');
-    
-        do{
-            montoSolicitado = parseInt(prompt('Ingrese el monto que desea solicitar'));
-        } while(isNaN(montoSolicitado))
-        
-        while(montoSolicitado < 5000){
-            montoSolicitado = parseInt(prompt('El monto solicitado debe ser mayor a $5000'));
-        }
-        
-        while((montoSolicitado % 500) != 0){
-            montoSolicitado = parseInt(prompt('El monto solicitado debe ser múltiplo de $500'));
-        }
-
-        while(usuarioGuardado.includes(dni) && usuarioGuardado[2].montoSolicitado === montoSolicitado){
-            if(confirm('Ya realizó una solicitud recientemente por el mismo importe\n\n¿Desea continuar?')){
-            } else{
-                simularCredito();
-            }
-        }
-        
-        do{
-            opcionCuotas = parseInt(prompt('¿En cuántas cuotas?\n\n1. 12 cuotas\n\n2. 24 cuotas\n\n3. 36 cuotas\n\n4. 48 cuotas\n\n5. 60 cuotas'));
-        } while(opcionCuotas < 1 || opcionCuotas > 5 || isNaN(opcionCuotas))
-    
     
         switch(opcionCuotas){
             case 1:
@@ -125,7 +233,7 @@ function simularCredito(){
     calcularMontos();
 }
 
-simularCredito();
+simularCredito(); */
 
 
 
